a29bbf7fc482ea2cd26e6d152bee7f95c579e273
atsushi130 <atsushi_main@outlook.jp>
2019-04-14 23:18:59 +0900

    implement: ansi escaped method for String as extension

--- Sources/AnsiEspaceSequenceSwift/String+AnsiEscapeSequence.swift
+++ Sources/AnsiEspaceSequenceSwift/String+AnsiEscapeSequence.swift
@@ -1,3 +1,43 @@
-struct AnsiEspaceSequenceSwift {
-    var text = "Hello, World!"
+//
+//  String+AnsiEscapeSequence.swift
+//  AnsiEscapeSequenceSwift
+//
+//  Created by Atsushi Miyake on 2019/04/14.
+//
+
+extension String {
+    
+    func addResetAnsiEscapeSequence() -> String {
+        return self + AnsiEscapeSequence.Attributed.reset.ansiEscapeSequence
+    }
+    
+    func ansiEscaped(by ansiEscapeSequence: [AnsiEscapeSequenceable], target: String?) -> String {
+        let ansiEscapeSequence = ansiEscapeSequence
+            .map { $0.ansiEscapeSequence }
+            .joined(separator: "")
+        if let target = target {
+            let ansiEscaped = (ansiEscapeSequence + target).addResetAnsiEscapeSequence()
+            return self.replacingOccurrences(of: target, with: ansiEscaped)
+        } else {
+            return (ansiEscapeSequence + self).addResetAnsiEscapeSequence()
+        }
+    }
+
+    public func ansiEscaped(by ansiEscapeSequence: AnsiEscapeSequence.Attributed..., target: String? = nil) -> String {
+        return self.ansiEscaped(by: ansiEscapeSequence, target: target)
+    }
+    
+    public func ansiEscaped(by ansiEscapeSequence: AnsiEscapeSequence.Cursor..., target: String? = nil) -> String {
+        return self.ansiEscaped(by: ansiEscapeSequence, target: target)
+    }
+    
+    public func ansiEscaped(by ansiEscapeSequence: String..., target: String? = nil) -> String {
+        return self.ansiEscaped(by: ansiEscapeSequence, target: target)
+    }
+}
+
+extension String: AnsiEscapeSequenceable {
+    var ansiEscapeSequence: String {
+        return self
+    }
 }
